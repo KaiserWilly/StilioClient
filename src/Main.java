c34871b5bd41c82a163ecb27d1019848c75ba64f
JD <jamesdisenhart@gmail.com>
2016-12-07 20:08:59 -0700

    Divided Query to Server and Client, created Client Object

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import network.Array;
+import network.InifQueryClient;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  * Created by JD Isenhart on 12/6/2016.
  * Testing RMI creation in Java 8
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String queryIP = args[0];
+        int queryPort = Integer.parseInt(args[1]);
+        try {
+            Registry registry = LocateRegistry.getRegistry(queryIP, queryPort); //IP Address of RMI Server, port of RMIRegistry
+            InifQueryClient stub = (InifQueryClient) registry.lookup("QueryClient");
+            Array server = stub.assignToArray();
+        } catch (Exception e) {
+            System.err.println("Unable to find Query Server!");
+        }
+
+
     }
 }
